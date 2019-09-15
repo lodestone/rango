9c0e260b7378ae436100dae99d26684fb26712de
Jan Biedermann <jan@kursator.de>
2019-09-15 21:17:25 +0200

    run in any case.

--- lib/arango/server/administration.rb
+++ lib/arango/server/administration.rb
@@ -174,7 +174,7 @@ module Arango
       # Returns the id of a server in a cluster.
       # @return [Boolean]
       def server_id
-        request("GET", "_api/replication/server-id", key: :serverId) if in_cluster?
+        request("GET", "_api/replication/server-id", key: :serverId)
       end
 
       # Returns the statistics information.
