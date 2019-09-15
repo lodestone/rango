a2e17ac49dc9c8befcc13b7eb662fe96b997802f
Jan Biedermann <jan@kursator.de>
2019-09-15 17:53:36 +0200

    Database complete with specs.

--- lib/arango/database.rb
+++ lib/arango/database.rb
@@ -7,7 +7,6 @@ module Arango
     include Arango::Helper::ServerAssignment
 
     include Arango::Database::AQLFunctions
-    include Arango::Database::Basics
     include Arango::Database::Collections
     include Arango::Database::FoxxServices
     include Arango::Database::GraphAccess
@@ -20,95 +19,140 @@ module Arango
     include Arango::Database::Transactions
     include Arango::Database::ViewAccess
 
-    # TODO see js api https://www.arangodb.com/docs/devel/drivers/js-reference-database.html
-    def initialize(name, server:)
+    class << self
+      # Retrieves all databases.
+      # @param server [Arango::Server]
+      # @return [Array<Arango::Database>]
+      def all(server:)
+        result = server.request("GET", "_api/database", key: :result)
+        result.map{ |db| Arango::Database.new(db, server: server).reload }
+      end
+
+      # Retrieves all databases the current user can access.
+      # @param server [Arango::Server]
+      # @return [Array<Arango::Database>]
+      def all_user_databases(server:)
+        result = server.request("GET", "_api/database/user", key: :result)
+        result.map{ |db| Arango::Database.new(db, server: server).reload }
+      end
+
+      # Get database from server.
+      # @param name [String] The name of the database
+      # @param server [Arango::Server]
+      # @return [Arango::Database] The instance of the database.
+      def get(name, server:)
+        Arango::Database.new(name, server: server).reload
+      end
+      alias fetch get
+      alias retrieve get
+
+      # Retrieves a list of all databases.
+      # @param server [Arango::Server]
+      # @return [Array<String>] List of database names.
+      def list(server:)
+        server.request("GET", "_api/database", key: :result)
+      end
+
+      # Retrieves a list of all databases the current user can access.
+      # @param server [Arango::Server]
+      # @return [Array<String>] List of database names.
+      def list_user_databases(server:)
+        server.request("GET", "_api/database/user", key: :result)
+      end
+
+      # Removes a database.
+      # @param name [String] The name of the database
+      # @param server [Arango::Server]
+      # @return nil
+      def drop(name, server:)
+        server.request("DELETE", "_api/database/#{name}")
+        nil
+      end
+      alias delete drop
+      alias destroy drop
+
+      # Check if database exists.
+      # @param name [String] Name of the database.
+      # @param server [Arango::Server]
+      # @return [Boolean]
+      def exist?(name, server:)
+        list(server: server).include?(name)
+      end
+    end
+
+    # Instantiate a new database. All params except name and server are optional.
+    # @param name [String]
+    # @param server [Arango::Server]
+    # @param id
+    # @param is_system
+    # @param path
+    # @return [Arango::Database]
+    def initialize(name, id: nil, is_system: false, path: '', server:)
       assign_server(server)
       @name = name
-      @server = server
-      @is_system = nil
-      @path = nil
-      @id = nil
+      @is_system = is_system
+      @path = path
+      @id = id
     end
 
-    def aquire_host_list
-      # TODO
+    # Whether or not the current database is the _system database
+    # @return [Boolean]
+    attr_reader :is_system
+
+    # The name of the database
+    # @return [String]
+    attr_reader :name
+
+    # The filesystem path of the current database
+    # @return [String]
+    attr_reader :path
+
+    attr_reader :server
+
+    # Creates the database on the server.
+    # @return [Arango::Database] self
+    def create
+      # TODO users: users
+      body = { name: @name }
+      @server.request("POST", "_api/database", body: body)
+      self
     end
 
-    def use_database
-      # TODO
+    # Reload database properties.
+    # @return [Arango::Database] self
+    def reload
+      result = request("GET", "_api/database/current", key: :result)
+      _update_attributes(result)
+      self
     end
+    alias refresh reload
+    alias retrieve reload
 
-    def use_basic_auth
-      # TODO
+    # Remove database from the server.
+    # @return nil
+    def drop
+      self.class.drop(@name, server: @server)
     end
+    alias delete drop
+    alias destroy drop
 
-    def use_bearer_auth
-      # TODO
-    end
-
-    def login
-      # TODO
-    end
-
+    # Returns the database version that this server requires.
+    # @return [String]
     def target_version
-
+      request("GET", "_admin/database/target-version", key: :version)
     end
 
-    def version
-      # TODO
-    end
-
-    def close
-      # TODO
-    end
-
-    # === DEFINE ===
-    attr_reader :cache_name, :id, :is_system, :path, :server
-    attr_accessor :name
-
-    # === TO HASH ===
-    # why?
-    def to_h
-      {
-        name:     @name,
-        isSystem: @is_system,
-        path:     @path,
-        id:       @id,
-        cache_name: @cache_name,
-        server: @server.base_uri
-      }.delete_if{|k,v| v.nil?}
-    end
-
-    # === REQUEST ===
-    def request(action, url, body: {}, headers: {},
-      query: {}, key: nil, return_direct_result: false,
-      skip_to_json: false, keep_null: false)
+    def request(action, url, body: {}, headers: {}, query: {}, key: nil, skip_to_json: false, keep_null: false)
       url = "_db/#{@name}/#{url}"
-      @server.request(action, url, body: body, headers: headers, query: query, key: key, skip_to_json: skip_to_json,
-                      keep_null: keep_null)
+      @server.request(action, url, body: body, headers: headers, query: query, key: key, skip_to_json: skip_to_json, keep_null: keep_null)
     end
 
-# === USER ACCESS ===
+    private
 
-    def check_user(user)
-      user = Arango::User.new(user: user) if user.is_a?(String)
-      return user
-    end
-    private :check_user
-
-    def add_user_access(grant:, user:)
-      user = check_user(user)
-      user.add_database_access(grant: grant, database: @name)
-    end
-
-    def revoke_user_access(user:)
-      user = check_user(user)
-      user.revoke_database_access(database: @name)
-    end
-
-    def user_access(user:)
-      user = check_user(user)
-      user.database_access(database: @name)
+    def _update_attributes(result)
+      %i[id isSystem name path].each do |key|
+        instance_variable_set("@#{key.to_s.underscore}", result[key]) if result.key?(key)
+      end
     end
   end
 end
---
+++ spec/database_spec.rb
@@ -0,0 +1,185 @@
+require 'spec_helper'
+
+describe Arango::Database do
+  before :all do
+    @server = connect
+  end
+
+  before :each do
+    begin
+      @server.drop_database("MyDatabase")
+    rescue
+    end
+  end
+
+  after :each do
+    begin
+      @server.drop_database("MyDatabase")
+    rescue
+    end
+  end
+
+  context "Server" do
+    it "new_database" do
+      database = @server.new_database "MyDatabase"
+      expect(database.name).to eq "MyDatabase"
+    end
+
+    it "create_database" do
+      database = @server.create_database "MyDatabase"
+      expect(database.name).to eq "MyDatabase"
+    end
+
+    it "fails to create a duplicate Database" do
+      @server.create_database "MyDatabase"
+      error = nil
+      begin
+        @server.create_database "MyDatabase"
+      rescue Arango::Error => e
+        error = e.message
+      end
+      expect(error).to eq "duplicate name"
+    end
+
+    it "list_databases" do
+      @server.create_database "MyDatabase"
+      list = @server.list_databases
+      expect(list).to include("MyDatabase")
+    end
+
+    it "list_user_databases" do
+      @server.create_database "MyDatabase"
+      list = @server.list_user_databases
+      expect(list).to include("MyDatabase")
+    end
+
+    it "drop_database" do
+      @server.create_database "MyDatabase"
+      list = @server.list_databases
+      expect(list).to include("MyDatabase")
+      @server.drop_database("MyDatabase")
+      list = @server.list_databases
+      expect(list).not_to include("MyDatabase")
+    end
+
+    it "exist_database?" do
+      expect(@server.exist_database?("MyDatabase")).to be false
+      @server.create_database "MyDatabase"
+      expect(@server.exist_database?("MyDatabase")).to be true
+    end
+
+    it "all_databases" do
+      @server.create_database "MyDatabase"
+      alldb = @server.all_databases
+      expect(alldb.map(&:name)).to include("MyDatabase")
+    end
+
+    it "all_user_databases" do
+      @server.create_database "MyDatabase"
+      alldb = @server.all_user_databases
+      expect(alldb.map(&:name)).to include("MyDatabase")
+    end
+
+    it "get_database" do
+      @server.create_database "MyDatabase"
+      database = @server.get_database("MyDatabase")
+      expect(database.name).to eq "MyDatabase"
+    end
+
+    it "target_version" do
+      database = @server.create_database "MyDatabase"
+      expect(database.target_version).to be_a String
+    end
+  end
+
+  # context "#info" do
+  #   it "obtain general info" do
+  #     @database.retrieve
+  #     expect(@database.name).to eq "MyDatabase"
+  #   end
+  # end
+
+  # context "#query" do
+  #   it "properties" do
+  #     expect(@database.query_properties[:enabled]).to be true
+  #   end
+  #
+  #   it "current" do
+  #     expect(@database.current_query.empty?).to be true
+  #   end
+  #
+  #   it "slow" do
+  #     expect(@database.slow_queries.empty?).to be true
+  #   end
+  # end
+
+  # context "#delete query" do
+  #   it "stopSlow" do
+  #     expect(@database.stop_slow_queries).to be true
+  #   end
+  #
+  #   # it "kill" do
+  #   #   @myCollection.create
+  #   #   @myCollection.createDocuments document: [{num: 1, _key: "FirstKey"},
+  #   #     {num: 1}, {num: 1}, {num: 1}, {num: 1}, {num: 1},
+  #   #     {num: 1}, {num: 2}, {num: 2}, {num: 2}, {num: 3},
+  #   #     {num: 2}, {num: 5}, {num: 2}]
+  #   #   myAQL = @database.aql query: 'FOR i IN 1..1000000
+  #   # INSERT { name: CONCAT("test", i) } IN MyCollection'
+  #   #   myAQL.size = 3
+  #   #   myAQL.execute
+  #   #   error = ""
+  #   #   begin
+  #   #     @database.killAql query: myAQL
+  #   #   rescue Arango::Error => e
+  #   #     error = e.message
+  #   #   end
+  #   #   expect(error.include?("It could have already been killed")).to eq true
+  #   # end
+  #
+  #   it "changeProperties" do
+  #     result = @database.change_query_properties max_slow_queries: 65
+  #     expect(result[:maxSlowQueries]).to eq 65
+  #   end
+  # end
+
+  # context "#cache" do
+  #   it "clear" do
+  #     expect(@database.clear_query_cache).to be true
+  #   end
+  #
+  #   it "change Property Cache" do
+  #     @database.change_query_properties max_slow_queries: 130
+  #     expect(@database.query_properties[:maxSlowQueries]).to eq 130
+  #   end
+  # end
+
+  # context "#function" do
+  #   it "create Function" do
+  #     result = @database.create_aql_function name: "myfunctions::temperature::celsiustofahrenheit",
+  #     code: "function (celsius) { return celsius * 1.8 + 32; }"
+  #     expect(result.class).to eq Hash
+  #   end
+  #
+  #   it "list Functions" do
+  #     result = @database.aql_functions
+  #     expect(result[0][:name]).to eq "myfunctions::temperature::celsiustofahrenheit"
+  #   end
+  #
+  #   it "delete Function" do
+  #     result = @database.delete_aql_function name: "myfunctions::temperature::celsiustofahrenheit"
+  #     expect(result).to be true
+  #   end
+  # end
+
+
+  # context "#user" do
+  #   it "grant" do
+  #     expect(@database.add_user_access(grant: "ro", user: @myUser)).to eq "ro"
+  #   end
+  #
+  #   it "revoke" do
+  #     expect(@database.revoke_user_access user: @myUser).to be true
+  #   end
+  # end
+end
