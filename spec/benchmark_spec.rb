fd12345173593f7b7bddefc910149ecb292aef16
Jan Biedermann <jan@kursator.de>
2019-09-26 02:50:11 +0200

    Fix bug in benchmark.

--- spec/benchmark_spec.rb
+++ spec/benchmark_spec.rb
@@ -1,6 +1,8 @@
 require_relative 'spec_helper'
 require 'benchmark'
 
+NUMBER_OF_RUNS = 5000
+
 describe Arango::Server do
   context "benchmark http" do
     before :all do
@@ -28,91 +30,86 @@ describe Arango::Server do
 
     it "doc benchmark in kops" do
       result = nil
-      n = 10000
       elapsed = Benchmark.realtime do
         # n sets, n get
-        n.times do |i|
+        NUMBER_OF_RUNS.times do |i|
           key = "foo#{i}"
           @collection.create_document({key: key, value: key * 10})
           result = @collection.get_document(key).value
         end
       end
-      n.times do |i|
+      NUMBER_OF_RUNS.times do |i|
         key = "foo#{i}"
         @collection.drop_document(key)
       end
-      puts 'Write/Read: %.2f Kops' % (2 * n / 1000 / elapsed)
+      puts 'Write/Read: %.2f Kops' % (2 * NUMBER_OF_RUNS / 1000 / elapsed)
       expect(result).to be_a String
     end
 
     it "doc benchmark in kops batched" do
       result = nil
-      n = 10000
       elapsed = Benchmark.realtime do
         # n sets, n get
-        n.times do |i|
+        NUMBER_OF_RUNS.times do |i|
           key = "foo#{i}"
           @collection.batch_create_document({key: key, value: key * 10}, wait_for_sync: false)
           @collection.batch_get_document(key).then { |doc| result = doc.value }
           @collection.database.execute_batched_requests
         end
       end
-      n.times do |i|
+      NUMBER_OF_RUNS.times do |i|
         key = "foo#{i}"
         @collection.drop_document(key)
       end
-      puts 'Write/Read batched: %.2f Kops' % (2 * n / 1000 / elapsed)
+      puts 'Write/Read batched: %.2f Kops' % (2 * NUMBER_OF_RUNS / 1000 / elapsed)
       expect(result).to be_a String
     end
 
     it "doc write benchmark in kops" do
-      n = 10000
       elapsed = Benchmark.realtime do
         # n sets, n get
-        n.times do |i|
+        NUMBER_OF_RUNS.times do |i|
           key = "foo#{i}"
           @collection.create_document({key: key, value: key * 10})
         end
       end
-      n.times do |i|
+      NUMBER_OF_RUNS.times do |i|
         key = "foo#{i}"
         @collection.drop_document(key)
       end
-      puts 'Write: %.2f Kops' % (2 * n / 1000 / elapsed)
+      puts 'Write: %.2f Kops' % (NUMBER_OF_RUNS / 1000 / elapsed)
     end
 
     it "doc read benchmark in kops" do
       result = nil
-      n = 10000
-      n.times do |i|
+      NUMBER_OF_RUNS.times do |i|
         key = "foo#{i}"
         @collection.create_document({key: key, value: key * 10})
       end
       elapsed = Benchmark.realtime do
         # n sets, n get
-        n.times do |i|
+        NUMBER_OF_RUNS.times do |i|
           key = "foo#{i}"
           result = @collection.get_document(key).value
         end
       end
-      n.times do |i|
+      NUMBER_OF_RUNS.times do |i|
         key = "foo#{i}"
         @collection.drop_document(key)
       end
-      puts 'Read: %.2f Kops' % (2 * n / 1000 / elapsed)
+      puts 'Read: %.2f Kops' % (NUMBER_OF_RUNS / 1000 / elapsed)
       expect(result).to be_a String
     end
 
     it "version benchmark in kops" do
       result = nil
-      n = 10000
       elapsed = Benchmark.realtime do
         # n sets, n get
-        n.times do |i|
+        NUMBER_OF_RUNS.times do |i|
           result = @server.version
         end
       end
-      puts 'version: %.2f Kops' % (2 * n / 1000 / elapsed)
+      puts 'version: %.2f Kops' % (NUMBER_OF_RUNS / 1000 / elapsed)
       expect(result).to be_a String
     end
   end
