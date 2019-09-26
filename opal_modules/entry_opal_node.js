c72d49e876321e348ee6bbe9e8ebbf52f092b884
Jan Biedermann <jan@kursator.de>
2019-09-26 08:25:02 +0200

    Remove node module, doesnt work, fix promise load.

--- opal_modules/entry_opal_node.js
+++ opal_modules/entry_opal_node.js
@@ -1,10 +1,8 @@
 import opal from 'opal.rb';
 opal();
 Opal.load('opal');
-import opal_node from 'nodejs.rb';
-opal_node();
-Opal.load('nodejs');
-// make promise available by default, used by require_lazy
 import promise from 'promise.rb'
+promise();
 Opal.load('promise');
-export default Opal;
+
+exports.Opal = Opal;
