28435644e2b4ff418c0df79f8c2c8a156a3aad62
Jan Biedermann <jan@kursator.de>
2019-09-27 04:27:28 +0200

    explicitly import native.rb, arangos module system is not perfect.

--- opal_modules/entry_opal_node.js
+++ opal_modules/entry_opal_node.js
@@ -1,6 +1,9 @@
 import opal from 'opal.rb';
 opal();
 Opal.load('opal');
+import native from 'native.rb'
+native();
+Opal.load('native');
 import promise from 'promise.rb'
 promise();
 Opal.load('promise');
