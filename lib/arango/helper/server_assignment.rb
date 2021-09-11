003976d326ace0ff3feea5ce037418a1e7d79e3b
Klaus Kämpf <kkaempf@suse.de>
2021-09-11 09:04:42 +0200

    add ServerAssignment helper
    Signed-off-by: Klaus Kämpf <kkaempf@suse.de>

---
+++ lib/arango/helper/server_assignment.rb
@@ -0,0 +1,13 @@
+module Arango
+  module Helper
+    module ServerAssignment
+
+      protected
+
+      def assign_server(server)
+        satisfy_class?(server, Arango::Server)
+        @server = server
+      end
+    end
+  end
+end
